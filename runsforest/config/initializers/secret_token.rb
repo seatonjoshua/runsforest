795ffe6dc5020a69a4fc7390aa2929182904d619
Joshua Seaton <joshuaseaton@Joshua-Seatons-MacBook-Pro.local>
2014-02-04 15:57:44 -0500

    first commit

---
+++ runsforest/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Runsforest::Application.config.secret_key_base = 'cbca9b8a8631f72835fba58ee1aa419a551fef8072f144fd1c52ee042f1596a9b2e0347b6f200cac90e57cdb3d4faf8150a32bba6e90147fb87e0b1b46cd95b5'
---
+++ runsforest/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Runsforest::Application.config.session_store :cookie_store, key: '_runsforest_session'
